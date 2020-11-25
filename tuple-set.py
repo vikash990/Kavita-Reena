e5500b1a80dd1169946d1e2c09ba56f97cfed856
aman dubey <dubey.amansus@gmail.com>
2020-11-25 09:59:46 -0800

    tuple-set

---
+++ tuple-set.py
@@ -0,0 +1,15 @@
+a=[100,80,65,54,25,50,45]
+print(a)
+a.insert(5,88)
+a.insert(6,99)
+print(a)
+name=["kavita","heena","sony","deepu","kavya","kriti"]
+print(name)
+name.insert(4,"soumya")
+print(name)
+fruits=("mango","apple","grapes","orange","banana")
+print("the number of fruits",len(fruits))
+a={90,90,90,90,85,60,93,42,87,65,}
+print(a)
+name={"kavita","kavita","kavita","heena","sony","deepu"}
+print(name)
